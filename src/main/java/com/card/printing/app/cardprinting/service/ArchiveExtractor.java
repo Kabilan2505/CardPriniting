5c5691e50db59e83e256e4b5254b6b7855f8ffa4
kabilan2505 <kabilankannan2505@gmail.com>
2024-07-10 21:17:08 +0530

    decrypt zip

--- src/main/java/com/card/printing/app/cardprinting/service/ArchiveExtractor.java
+++ src/main/java/com/card/printing/app/cardprinting/service/ArchiveExtractor.java
@@ -1,29 +1,29 @@
 package com.card.printing.app.cardprinting.service;
 
+import java.io.*;
+import java.nio.Buffer;
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
 import com.card.printing.app.cardprinting.common.EncryptData;
 import com.card.printing.app.cardprinting.common.InitConstants;
 import com.github.junrar.Archive;
 import com.github.junrar.exception.RarException;
 import com.github.junrar.rarfile.FileHeader;
 import net.lingala.zip4j.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.util.zip.ZipException;
-
-
 @Service
 public class ArchiveExtractor {
 
-    @Autowired
-    EncryptData encryptData;
+
+    EncryptData encryptData = new EncryptData();
 
     Logger log = LoggerFactory.getLogger(ArchiveExtractor.class);
 
@@ -32,11 +32,16 @@ public class ArchiveExtractor {
             if(file.isFile()){
                 String fileName = file.getName().toLowerCase();
                 if(fileName.endsWith(".zip")){
-                    extractZip(file, String.valueOf(Paths.get(output, InitConstants.unzip)));
-                    moveZiporRarFile(file , String.valueOf(Paths.get(output , InitConstants.extractedZip)));
+                    String isTxtFile = fileName.replaceFirst("\\.zip$",".txt");
+                    File txtFile = new File(file.getParent() , isTxtFile);
+                    if (txtFile.exists() && txtFile.isFile()){
+                        System.out.println(txtFile.getName());
+                    }
+                    extractZip(file, String.valueOf(Paths.get(output, InitConstants.unzip)) , txtFile);
+//                    moveZiporRarFile(file , String.valueOf(Paths.get(output , InitConstants.extractedZip)));
                 }else if(fileName.endsWith(".rar")){
                     extractRar(file, String.valueOf(Paths.get(output, InitConstants.unrar)));
-                    moveZiporRarFile(file , output+InitConstants.extractedZip);
+//                    moveZiporRarFile(file , output+InitConstants.extractedZip);
                 }else if(fileName.endsWith(".txt")){
                     decryptPCN(file);
                 }
@@ -59,11 +64,11 @@ public class ArchiveExtractor {
     }
 
    public void moveZiporRarFile(File file , String outputDir){
-        System.out.println("File "+file.toString());
+        System.out.println("File " + file.toString());
         Path sourcePath = file.toPath();
         Path movingPath = new File(outputDir , file.getName()).toPath();
         try{
-            if(!Files.exists(movingPath)){
+            if(!Files.exists(movingPath) && !(file.getName().endsWith(".txt"))){
                 Files.createDirectories(movingPath);
             }
             Files.move(sourcePath , movingPath , StandardCopyOption.REPLACE_EXISTING);
@@ -72,53 +77,119 @@ public class ArchiveExtractor {
         }
    }
 
-    private void extractZip(File file, String outputDir) {
+    private void extractZip(File file, String outputDir , File txtFilePath) {
         try {
             String outputPath = Paths.get(outputDir , file.getName().replace(".zip","")).toString();
             File outputFile = new File(outputPath);
             outputFile.mkdirs();
+//            moveZiporRarFile(txtFilePath , outputPath);
             ZipFile zipFile = new ZipFile(file);
             zipFile.extractAll(outputPath);
-            processFiles(new File(outputDir));
-        } catch (net.lingala.zip4j.exception.ZipException e) {
-            throw new RuntimeException(e);
+            processFiles(new File(outputPath , file.getName().replaceFirst("\\.zip$","")));
+        } catch (ZipException e) {
+            e.printStackTrace();
         }
     }
 
     private void processFiles(File outputFolder) {
-        if (outputFolder.isDirectory()) {
+        System.out.println(outputFolder + " ----- ");
+//        if (outputFolder.isDirectory()) {
+            File text = new File(outputFolder.getAbsolutePath());
+            File parentFile = text.getParentFile();
+//            System.out.println("text File " + text.getName());
+//
+            List<String> pcns = readPcns(new File(parentFile , text.getName() + ".txt" ));
+
             File[] files = outputFolder.listFiles();
+            StringBuilder builder = new StringBuilder();
             if (files != null) {
-                StringBuilder builder = new StringBuilder();
                 for (File file : files) {
                     if (file.isDirectory()) {
                         processFiles(file);
                     } else if (file.getName().endsWith(".txt")) {
-                        System.out.println("Processing file: " + file.getAbsolutePath());
-                        decryptPCN(file);
+
+                        String filename = file.getName().toLowerCase().replace(".txt","");
+
+//                        boolean exists = pcns.contains(filename);
+
+                        System.out.println(filename);
+
+                        boolean isDecrypted =  decryptPCN(file);
+
+//                        builder.append(filename).append(exists ? " - Exists " : " - Not Exists ").append("\n");
+
+                        builder.append(filename).append(isDecrypted ? " - Processed " : " - Not Processed ").append("\n");
+
+                        pcns.remove(filename);
+
                     }
                 }
             }
+
+            for(String remainingPcns : pcns){
+                builder.append(remainingPcns).append(" - Not Found \n");
+            }
+
+            updateTextFile(builder.toString() , new File(parentFile , text.getName() + ".txt" ));
+
+//        }
+    }
+
+    private void updateTextFile(String content , File file){
+        try(PrintWriter printWriter = new PrintWriter(new FileWriter(file))){
+                printWriter.write(content);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
+    private List<String> readPcns(File file){
+        List<String> pcnList = new ArrayList<>();
 
-    private void decryptPCN(File file){
         try{
-            System.out.println(file);
             BufferedReader reader = new BufferedReader(new FileReader(file));
-            String line = reader.readLine();
-            String[] diltSimpleAfter = line.split("~");
-            if(diltSimpleAfter.length > 1) {
-                String afterDelimiter = diltSimpleAfter[0].trim();
-                String decrpt = encryptData.AESDecrypt(afterDelimiter);
-                System.out.println(decrpt);
+            String line;
+            while((line = reader.readLine()) != null){
+                line = line.trim();
+                if(!line.isEmpty()){
+                    pcnList.add(line);
+                }
             }
+
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+        return pcnList;
+    }
+
+
+    private boolean decryptPCN(File file){
+        System.out.println(file.getName());
+        String decrpt = null;
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line = reader.readLine();
+            String[] diltSimpleAfter = line.split("~");
+            if(diltSimpleAfter.length > 0) {
+                String afterDelimiter = diltSimpleAfter[0].trim();
+                decrpt = encryptData.AESDecrypt(afterDelimiter);
+                System.out.println(decrpt);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(decrpt.isEmpty() || decrpt == null){
+            return false;
+        }
+
+        return true;
+    }
+
+    public void processTextFile(File file , String content){
+
     }
 
     /*private static void extractZip(File zipFile, String outputDir) {
@@ -176,7 +247,7 @@ public class ArchiveExtractor {
             }
             zipFile.extractAll(output);
             System.out.println("Extraction Successfully");
-        } catch (net.lingala.zip4j.exception.ZipException e) {
+        } catch (ZipException e) {
             throw new RuntimeException(e);
         }
     }
