0d33e4646adc7289a64eac85eb473f577d7e9808
Naveensekaran <naveen.sekaran01@gmail.com>
2024-07-12 05:52:18 +0530

    batch and demand completed

--- src/main/java/com/card/printing/app/cardprinting/PreviewController.java
+++ src/main/java/com/card/printing/app/cardprinting/PreviewController.java
@@ -6,8 +6,10 @@ import com.card.printing.app.cardprinting.service.ArchiveExtractor;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.AnchorPane;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -64,7 +66,10 @@ public class PreviewController {
         Image imgae=new Image(is);
         System.out.println(res);
         colorphoto.setImage(imgae);
-//        id.setText(res.getId());
+        Image blackNwhite=convertToGrayscale(imgae);
+        bwphoto.setImage(blackNwhite);
+        id.setText(res.getId());
+
 
 
         creationDate.setText(res.getCreationDate());
@@ -76,5 +81,24 @@ public class PreviewController {
         Image imgae1=new Image(is1);
         qrcode.setImage(imgae1);
         id1.setText(res.getId());
+    }
+
+    public static Image convertToGrayscale(Image image) {
+        ImageView imageView = new ImageView(image);
+
+        // Create a ColorAdjust effect
+        ColorAdjust colorAdjust = new ColorAdjust();
+        colorAdjust.setSaturation(-1); // Set saturation to -1 to remove color (grayscale effect)
+
+        // Apply the effect to the ImageView
+        imageView.setEffect(colorAdjust);
+
+        // Create a WritableImage to hold the processed image
+        WritableImage grayscaleImage = new WritableImage((int) image.getWidth(), (int) image.getHeight());
+
+        // Snapshot the ImageView with the effect applied to the WritableImage
+        imageView.snapshot(null, grayscaleImage);
+
+        return grayscaleImage;
     }
 }
